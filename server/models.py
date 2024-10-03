61256f154494502e79f482652b3ca213a9383e6b
Kush <kuriaisac@gmail.com>
2024-10-03 20:00:17 +0300

    updates changes

--- server/models.py
+++ server/models.py
@@ -18,6 +18,12 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
+    
+    # mapping employee to related reviews
+    reviews = db.relationship('Review', back_populates='Employee')
+
+    # mapping employee to related onboarding
+    onboarding = db.relationship('Onboarding', uselist=False, back_populates='employee')
 
     def __repr__(self):
         return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
@@ -30,6 +36,11 @@ class Onboarding(db.Model):
     orientation = db.Column(db.DateTime)
     forms_complete = db.Column(db.Boolean, default=False)
 
+    # foreign key storing the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # mappimg onboarding to related employee
+    employee = db.relationship('Employee', back_populates='onboarding')
     def __repr__(self):
         return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
 
@@ -40,6 +51,7 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer)
     summary = db.Column(db.String)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
 
     def __repr__(self):
         return f"<Review {self.id}, {self.year}, {self.summary}>"
